ef15d891a377e0e14f05a5512ff55a2033c4e00c
tbpatj <luebketj@gmail.com>
2022-03-07 12:46:18 -0800

    updated the player to be a class instead of just a bunch of global variables

---
+++ StickySlimeBall/World.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace StickySlimeBall
+{
+    class World
+    {
+        List<CollideLine> collides = new List<CollideLine>();
+        List<BallCollide> bCollides = new List<BallCollide>();
+        List<Particle> particles = new List<Particle>();
+        List<Platform> movingPlatforms = new List<Platform>();
+        List<InfoText> Information = new List<InfoText>();
+
+
+        Vector gravity = new Vector(0, -0.1);
+        bool friction = true;
+        bool canStick = false;
+    }
+}
